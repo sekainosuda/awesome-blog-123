75f837f0ed6ea92148ae29a46ab623f2ebf6d647
Yusuke Ito <yusuke@YusukesMBP.orihime.ne.jp>
2021-01-25 19:41:53 +0900

    Add ports in app.js, Add Procfile and add node version in package.json

--- app.js
+++ app.js
@@ -75,7 +75,10 @@ app.get("/posts/:postId", function(req, res){
 
 });
 
-let port = 3000;
-app.listen(port, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, () => {
   console.log("Server started on port " + port + ".");
 });
